4930a1b6bcafbf97cca92b9eebba7313eaf3edcd
Mohan SP <33540716+spmohansp@users.noreply.github.com>
2019-03-07 19:53:01 +0530

    Update master.blade.php

--- resources/views/thottakaran/layout/master.blade.php
+++ resources/views/thottakaran/layout/master.blade.php
@@ -11,7 +11,7 @@
     
     <link rel="shortcut icon" type="image/x-icon" href="{{ url('images/logo.jpg') }}" />
     
-    <title>Thottakaran @yield('title')</title>
+    <title>Thottakaaran @yield('title')</title>
     
     <meta name="viewport" content="width=device-width, initial-scale=1">
     
